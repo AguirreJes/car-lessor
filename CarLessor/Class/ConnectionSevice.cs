d6032e47a743a2f3ac246610060f1573f92399e9
Jesus Edgardo Aguirre <jesuspma06@gmail.com>
2020-04-03 16:58:04 -0500

    Ajuste a clase servicio

--- CarLessor/Class/ConnectionSevice.cs
+++ CarLessor/Class/ConnectionSevice.cs
@@ -9,42 +9,89 @@ namespace CarLessor
 {
     public class ConnectionSevice
     {
-        
         public static Boolean SignIn(String user, String pass)
         {
-            
             Boolean response = false;
-            String query = "select count(1) from carlessor.login where username = @username and password = @password";
-
-            MySqlCommand cmd;
-            cmd = new MySqlCommand(query, getConnection());
-            cmd.Parameters.AddWithValue("@username", user);
-            cmd.Parameters.AddWithValue("@password", pass);
-            cmd.ExecuteNonQuery();
-            Int32 countUser = Convert.ToInt32(cmd.ExecuteScalar());
-
-            if (countUser > 0)
+            try
             {
-                response = true;
+                MySqlConnection connectionBd = getConnection();
+                
+                string query = "select count(1) from carlessor.login where username = @username and password = @password";
+                MySqlCommand cmd;
+                cmd = new MySqlCommand(query, connectionBd);
+                cmd.Parameters.AddWithValue("@username", user);
+                cmd.Parameters.AddWithValue("@password", pass);
+                cmd.ExecuteNonQuery();
+                Int32 countUser = Convert.ToInt32(cmd.ExecuteScalar());
+                connectionBd.Close();
+                if (countUser > 0)
+                {
+                    response = true;
+                }
+            }
+            catch (Exception exc)
+            {
+                showMessage("Error Message", exc.Message);
             }
             return response;
         }
 
         public static void DetailInformation(GridView inventoryCar)
         {
-            String query = "select * from carlessor.autos";
-            MySqlCommand cmd;
-            cmd = new MySqlCommand(query, getConnection());
-            inventoryCar.DataSource = cmd.ExecuteReader();
-            inventoryCar.DataBind();
+            try
+            {
+                MySqlConnection connectionBd = getConnection();
+                string query = "select * from carlessor.autos";
+                MySqlCommand cmd;
+                cmd = new MySqlCommand(query, connectionBd);
+                inventoryCar.DataSource = cmd.ExecuteReader();
+                inventoryCar.DataBind();
+                connectionBd.Close();
+            }
+            catch (Exception exc)
+            {
+                showMessage("Error Message", exc.Message);
+            }
         }
 
-        public static MySqlConnection getConnection()
+        public static void updateDetail(string idcar, string quantityDays, string quantityCars)
+        {
+            try
+            {
+                MySqlConnection connectionBd = getConnection();
+            string query = "update carlessor.autos set diaalquilado = @diaalquilado, cantidadauto = @cantidadauto where idautos = @idautos";
+            MySqlCommand cmd;
+            cmd = new MySqlCommand(query, connectionBd);
+            cmd.Parameters.AddWithValue("@idautos", idcar);
+            cmd.Parameters.AddWithValue("@diaalquilado", quantityDays);
+            cmd.Parameters.AddWithValue("@cantidadauto", quantityCars);
+            cmd.ExecuteNonQuery();
+            connectionBd.Close();
+            }
+            catch (Exception exc)
+            {
+                showMessage("Error Message", exc.Message);
+            }
+        }
+
+        private static MySqlConnection getConnection()
         {
             MySqlConnection con;
             con = new MySqlConnection("Data Source=localhost;Database=carlessor;User ID=root;Password=rootnitch;port=3306");
-            con.Open();
+            try
+            {
+                con.Open();
+            }
+            catch (Exception exc)
+            {
+                showMessage("Error Message", exc.Message);
+            }
             return con;
+        }
+
+        private static void showMessage(string v, string message)
+        {
+            throw new NotImplementedException();
         }
 
     }
