4e27ba1a0a9c9bafec129d0265c4b4d00e2ad4a0
NicolePaez <62733372+NicolePaez@users.noreply.github.com>
2020-04-03 00:20:02 -0500

    Conexion DB

--- CarLessor/SignIn.aspx.cs
+++ CarLessor/SignIn.aspx.cs
@@ -16,11 +16,9 @@ namespace CarLessor
 
         protected void signIn_Click(object sender, EventArgs e)
         {
-            //Validar que devolvera BD como confirmacion
-            //Validar BD mostrar error si no existe de lo contrario redireccionar a la consulta de datos
             if (ConnectionSevice.SignIn(inputUser.Text, inputPassword.Text))
             {
-                Response.Redirect("Pagina de consulta");
+                Response.Redirect("~/DetailedInformation.aspx");
             }
             else
             {
