b78a538ace11e698a5f3ea5d05dbe7f26d7ae065
Jesus Edgardo Aguirre <jesuspma06@gmail.com>
2020-04-06 23:42:09 -0500

    Creacion de clase precio

---
+++ CarLessor/Class/Pricing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarLessor.Class
+{
+    public class Pricing
+    {
+        public double amount { get; set; }
+        public double discount { get; set; }
+    }
+}
